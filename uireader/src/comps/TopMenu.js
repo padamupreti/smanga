abf22e9a79dfe9b26341e63a74f256aa056e1bfd
padamupreti <59172926+padamupreti@users.noreply.github.com>
2023-04-14 21:20:38 +0545

    Only display top menu navigation buttons for items with siblings

--- uireader/src/comps/TopMenu.js
+++ uireader/src/comps/TopMenu.js
@@ -10,35 +10,38 @@ const TopMenu = ({ showMenus, itemInfo, itemType, identifer, restruct }) => {
         <div className={showMenus ? 'top-menu visible-menu' : 'top-menu'}>
             <p>{name}</p>
             <div>
-                {itemType === 'chapter' && (
-                    <button
-                        className="change re-toggle"
-                        onClick={() => restruct()}
-                    >
-                        <FaRedoAlt />
-                    </button>
+                {prevItem && nextItem && identifer && (
+                    <>
+                        {itemType === 'chapter' && (
+                            <button
+                                className="change re-toggle"
+                                onClick={() => restruct()}
+                            >
+                                <FaRedoAlt />
+                            </button>
+                        )}
+                        <Link
+                            to={nextItem ? `./../${nextItem}/` : '.'}
+                            className={`change change-left ${
+                                nextItem ? '' : 'disabled'
+                            }`}
+                        >
+                            <FaArrowLeft />
+                        </Link>
+                        <Link to="./../" className="change change-text">
+                            {'#' + identifer}
+                        </Link>
+
+                        <Link
+                            to={prevItem ? `./../${prevItem}/` : '.'}
+                            className={`change change-right ${
+                                prevItem ? '' : 'disabled'
+                            }`}
+                        >
+                            <FaArrowRight />
+                        </Link>
+                    </>
                 )}
-                <Link
-                    to={nextItem ? `./../${nextItem}/` : '.'}
-                    className={`change change-left ${
-                        nextItem ? '' : 'disabled'
-                    }`}
-                >
-                    <FaArrowLeft />
-                </Link>
-                {itemType !== 'CBZ' && identifer && (
-                    <Link to="./../" className="change change-text">
-                        {'#' + identifer}
-                    </Link>
-                )}
-                <Link
-                    to={prevItem ? `./../${prevItem}/` : '.'}
-                    className={`change change-right ${
-                        prevItem ? '' : 'disabled'
-                    }`}
-                >
-                    <FaArrowRight />
-                </Link>
                 <Link to="/" className="change change-spaced">
                     <FaHome />
                 </Link>
